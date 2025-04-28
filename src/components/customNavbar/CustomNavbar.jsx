a1091af6419e691a96f01c1debbba0a2f9c3a6ea
Justina-R <justina.rey2602@gmail.com>
2025-04-28 12:29:15 -0300

    Update in navbar component

--- src/components/customNavbar/CustomNavbar.jsx
+++ src/components/customNavbar/CustomNavbar.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
-import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
-import './Navbar.css';
+import React, { useState } from "react";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { FaUser, FaShoppingCart, FaSearch } from "react-icons/fa";
+import "./Navbar.css";
 
 const CustomNavbar = () => {
+  // manejamos el estado de "expanded" para definir si la navbar está abierta o no
   const [expanded, setExpanded] = useState(false);
 
   const handleToggle = () => {
@@ -14,34 +16,69 @@ const CustomNavbar = () => {
   };
 
   return (
-    <Navbar expanded={expanded} expand="lg" bg="light" variant="light" fixed="top" className="shadow-sm">
-      <Container fluid className='navbar-container'>
-        <Navbar.Toggle className='button-toggle' aria-controls="basic-navbar-nav" onClick={handleToggle} />
-
-        <Navbar.Collapse id="basic-navbar-nav" className="custom-navbar-collapse">
-          {/* Botón de cerrar */}
+    <Navbar expanded={expanded} expand="true" fixed="top">
+      {/* expand: cuándo está abierto el navbar */}
+      <Container fluid>
+        {/* aria-controls: indica qué elemento se verá afectado (por id) */}
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          onClick={handleToggle}
+        />
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="custom-navbar-collapse"
+        >
           <div className="close-btn" onClick={handleClose}>
             &times;
           </div>
-
           <Nav className="flex-column">
-            <Nav.Link href="#inicio" onClick={handleClose}>Inicio</Nav.Link>
-
-            <NavDropdown title="Productos" id="productos-dropdown" drop="end">
-              <NavDropdown.Item href="#todos" onClick={handleClose}>Todos los productos</NavDropdown.Item>
-              <NavDropdown.Item href="#zapatillas" onClick={handleClose}>Zapatillas</NavDropdown.Item>
-              <NavDropdown.Item href="#zapatos" onClick={handleClose}>Zapatos</NavDropdown.Item>
-              <NavDropdown.Item href="#pantuflas" onClick={handleClose}>Pantuflas</NavDropdown.Item>
+            <Nav.Link href="#inicio" onClick={handleClose}>
+              Inicio
+            </Nav.Link>
+            <NavDropdown title="Productos" id="productos-dropdown">
+              <NavDropdown.Item href="#todos" onClick={handleClose}>
+                Todos los productos
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#botas" onClick={handleClose}>
+                Botas
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#zapatillas" onClick={handleClose}>
+                Zapatillas
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#zapatos" onClick={handleClose}>
+                Zapatos
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#pantuflas" onClick={handleClose}>
+                Pantuflas
+              </NavDropdown.Item>
             </NavDropdown>
-
-            <Nav.Link href="#nosotros" onClick={handleClose}>Nosotros</Nav.Link>
-            <Nav.Link href="#faq" onClick={handleClose}>Preguntas frecuentes</Nav.Link>
-            <Nav.Link href="#contacto" onClick={handleClose}>Contacto</Nav.Link>
+            <Nav.Link href="#nosotros" onClick={handleClose}>
+              Nosotros
+            </Nav.Link>
+            <Nav.Link href="#faq" onClick={handleClose}>
+              Preguntas frecuentes
+            </Nav.Link>
+            <Nav.Link href="#contacto" onClick={handleClose}>
+              Contacto
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
+
+        <Navbar.Brand href="#home">
+          <img
+            src="../../../images/DoryShoes-Logo.jpg"
+            className="d-inline-block align-top"
+            alt="Logo de Dory Shoes"
+          />
+        </Navbar.Brand>
+        <div className="header-icons">
+          <FaSearch className="icon"/>
+          <FaUser className="icon" />
+          <FaShoppingCart className="icon"/>
+        </div>
       </Container>
     </Navbar>
-);
+  );
 };
 
 export default CustomNavbar;
