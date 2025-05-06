a1f501eb4e200cd69da181d515b582eef42d4faf
Lauzari <mlauramoyano1985@gmail.com>
2025-05-05 22:14:01 -0300

    adding footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './footer.css';  // Importar el archivo CSS
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div>
+        <div className="social-links">
+          <a
+            href="https://www.instagram.com/tu_cuenta_instagram"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-dark"
+          >
+            <i className="bi bi-instagram fs-4"></i>
+          </a>
+          <a
+            href="https://www.facebook.com/tu_cuenta_facebook"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-dark"
+          >
+            <i className="bi bi-facebook fs-4"></i>
+          </a>
+        </div>
+        <div>© 2025 Dory Shoes - Todos los derechos reservados.</div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
