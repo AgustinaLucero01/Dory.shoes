c668b5c429a2a172ab0621a294d7423a8e5d6d28
Justina-R <justina.rey2602@gmail.com>
2025-05-20 16:23:21 -0300

    Changes to merge with frontDev

--- dory-shoes-client/src/App.jsx
+++ dory-shoes-client/src/App.jsx
@@ -1,8 +1,43 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import CustomNavbar from './components/customNavbar/CustomNavbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import Home from './components/home/Home';
+
+
+import Footer from './components/footer/Footer';
+import Products from "./components/products/Products";
+import ProductDetail from "./components/productDetail/ProductDetail";
+/*import Login from './components/login/Login';
+import Registro from './components/registro/Registro';*/
 
 const App = () => {
   return (
-    <div></div>
+    <Router>
+       <div className="app-container d-flex flex-column min-vh-100">
+
+       
+        <CustomNavbar />
+
+        
+        <main className="flex-fill">
+          <Routes>
+            <Route path="/" element={<Home />} />
+           
+           
+            <Route path="/categoria/:categoria" element={<Products />} />
+           
+           
+            <Route path="/product/:id" element={<ProductDetail />} />
+          </Routes>
+        </main>
+
+       
+        <Footer />
+
+      </div>
+    </Router>
   );
 };
 
