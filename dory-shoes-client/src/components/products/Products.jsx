f0ec5acaa5bd8e88930f604a1bbfc32f8dab4c63
Lauzari <mlauramoyano1985@gmail.com>
2025-05-23 16:00:17 -0300

    add filter all products

--- dory-shoes-client/src/components/products/Products.jsx
+++ dory-shoes-client/src/components/products/Products.jsx
@@ -1,27 +1,42 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import { useParams, Link } from 'react-router-dom';
 import ProductData from '../../data/products.json';
 import './Products.css';
 
-
 function Products() {
   const { categoria } = useParams();
+const [products, setProducts] = useState([]);
 
-  // Filtra los productos por la categoría
-  const productosFiltrados = ProductData.filter((producto) =>
-    producto.categoria.toLowerCase() === categoria.toLowerCase()
-  );
+useEffect(() => {
+    fetchProducts();
+     
+  }, []);
 
+  // Si no hay categoría en la URL, mostramos todos los productos
+  const productosFiltrados = categoria
+    ? products.filter(
+      (producto) =>
+        producto.categoria.toLowerCase() === decodeURIComponent(categoria).toLowerCase()
+    )
+    : products;
+
+  const fetchProducts = async () => {
+    fetch("http://localhost:3000/products")
+      .then((res) => res.json())
+      .then((data) => setProducts([...data]))
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="products-container">
       <div className="container mt-5">
-        <h2 className="text-center mb-4">{categoria}</h2>
+        <h2 className="text-center mb-4">
+          {categoria ? decodeURIComponent(categoria) : 'Todos los productos'}
+        </h2>
         <div className="row">
           {productosFiltrados.length > 0 ? (
             productosFiltrados.map((producto) => (
               <div key={producto.id} className="col-md-3 mb-4">
                 <div className="card">
-
                   <img
                     src={producto.imagen}
                     alt={producto.nombre}
@@ -30,26 +45,23 @@ function Products() {
                   />
                   <div className="card-body">
                     <h5 className="card-title">{producto.nombre}</h5>
-                    <p className="card-text">${producto.precio.toLocaleString('es-AR')}</p>
-                    {/* Enlace al detalle del producto */}
+                    <p className="card-text">
+                      ${producto.precio.toLocaleString('es-AR')}
+                    </p>
                     <Link to={`/product/${producto.id}`} className="btn btn-ver-detalle">
                       Ver detalle
                     </Link>
-
-                  
+                  </div>
                 </div>
               </div>
-              </div>
-        ))
-        ) : (
-        <p>No hay productos disponibles en esta categoría.</p>
+            ))
+          ) : (
+            <p>No hay productos disponibles.</p>
           )}
+        </div>
       </div>
     </div>
-    </div >
   );
 }
 
 export default Products;
-
-
