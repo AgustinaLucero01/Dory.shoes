d6958a601190d9ef5cf2410bf450162c5015b38d
Justina-R <justina.rey2602@gmail.com>
2025-05-27 07:56:27 -0300

    Solving stash problems

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css'
+import CustomNavbar from './components/customNavbar/CustomNavbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Productos from './components/products/Products';
+
+const App = () => {
+  return (
+    <Router>
+      <CustomNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/productos/:categoria" element={<Productos />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
