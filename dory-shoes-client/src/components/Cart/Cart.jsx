e0a8c3e280124a5de0dea44103554027ba5bf0a9
AgustinaLucero01 <agusslucero142005@gmail.com>
2025-05-28 23:06:41 -0300

    updated cart

--- dory-shoes-client/src/components/Cart/Cart.jsx
+++ dory-shoes-client/src/components/Cart/Cart.jsx
@@ -1,33 +1,51 @@
 import React, { useState, useEffect } from "react";
 import { FaShoppingCart } from "react-icons/fa";
+import { useContext } from "react";
+import {useNavigate} from "react-router-dom";
+import { CartContext } from "../Service/CartContext/CartContext";
 import "./cart.css";
 
-const Cart = ({Allproduct = [],setAllproduct, Total,CountProduct,setCountProduct,setTotal}) => {
+const Cart = () => {
+  const {
+    total,
+    setTotal,
+    countProduct,
+    setCountProduct,
+    Allproduct,
+    setAllproducts,
+  } = useContext(CartContext);
+
   const [active, setActive] = useState(false);
-  
-  const onDeleteProduct = (product) =>{
-    const result =Allproduct.filter(item => item.id !== product.id);
-     setAllproduct(result);
-     setTotal(Total - product.precio * product.quantity);
-     setCountProduct(CountProduct - product.quantity);
+
+  const navigate = useNavigate();
+
+  const onDeleteProduct = (product) => {
+    const result = Allproduct.filter(
+      (item) => !(item.id === product.id && item.size === product.size)
+    );
+    setAllproducts(result);
+    setTotal(total - product.precio * product.quantity);
+    setCountProduct(countProduct - product.quantity);
   };
 
 
-  const onclickCart = () =>{
-    setAllproduct([]);
+  const onclickCart = () => {
+    setAllproducts([]);
     setTotal(0);
     setCountProduct(0);
   }
 
-  
+  const handlenav = () => {
+    navigate("/shopping");
+  }
+
+
 
   return (
     <div>
       <div className="icon-cart">
-       <FaShoppingCart onClick={() => setActive(!active)} className="icon-cart-product" /> 
-        <div className="count-product">
-          <span id="count-product"  onClick={() => setActive(!active)}> {CountProduct}</span>
-        </div>
+        <FaShoppingCart onClick={() => setActive(!active)} className="icon-cart" />
+
       </div>
 
       <div className={`container-cart-products ${active ? '' : 'hidden-cart'}`}>
@@ -35,10 +53,10 @@ const Cart = ({Allproduct = [],setAllproduct, Total,CountProduct,setCountProduct
           <>
             <div className='row-product'>
               {Allproduct.map(product => (
-                <div className='cart-product' key={product.id}>
+                <div className='cart-product' key={product.id}> {/* key es para que cada producto se renderice una vez */}
                   <div className='info-cart-product'>
-                    <span className='cantidad-producto-carrito'> 
-                      {product.quantity} 
+                    <span className='cantidad-producto-carrito'>
+                      {product.quantity}
                     </span>
                     <p className='titulo-producto-carrito'>
                       {product.nombre}
@@ -47,21 +65,21 @@ const Cart = ({Allproduct = [],setAllproduct, Total,CountProduct,setCountProduct
                       ${product.precio}
                     </span>
                   </div>
-                   <button onClick={() => onDeleteProduct(product)}>Eliminar</button> 
+                  <button onClick={() => onDeleteProduct(product)}>Eliminar</button>
                 </div>
               ))}
             </div>
             <div className="cart-total">
               <h3>Total:</h3>
-              <span>${Total}</span>
+              <span>${total}</span>
             </div>
             <button className="btn-clear-all" onClick={onclickCart}>Vaciar carrito</button>
-            <button>Finalizar pedido</button>
+            <button onClick={handlenav}>Finalizar pedido</button>
           </>
         ) : (
           <p>El carrito está vacío</p>
-        )} 
-      </div> 
+        )}
+      </div>
     </div>
   );
 };
