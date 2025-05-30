0fbe0564a012b292ceee33ff1ff0a7b76c855524
Justina-R <justina.rey2602@gmail.com>
2025-05-30 14:24:56 -0300

    Modify App.jsx

--- dory-shoes-client/src/App.jsx
+++ dory-shoes-client/src/App.jsx
@@ -1,42 +1,41 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import CustomNavbar from './components/customNavbar/CustomNavbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import Home from './components/home/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
+// Layout
+import Layout from "./components/layout/Layout";
 
-import Footer from './components/footer/Footer';
+// Páginas
+import Home from "./components/home/Home";
 import Products from "./components/products/Products";
 import ProductDetail from "./components/productDetail/ProductDetail";
-/*import Login from './components/login/Login';
-import Registro from './components/registro/Registro';*/
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+import NotFound from "./components/errors/NotFound";
+import EditProfile from "./components/editProfile/EditProfile"
 
 const App = () => {
   return (
     <Router>
-       <div className="app-container d-flex flex-column min-vh-100">
-
-       
-        <CustomNavbar />
-
+      <Routes>
+        {/* Rutas que usan el Layout común */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/categoria/:categoria" element={<Products />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/register" element={<Register role={"user"} isEdit={false} />} />
+          <Route path="/editProfile/:id" element={<EditProfile/>} />
+          <Route path="/dashboard/*" element={<Dashboard />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
         
-        <main className="flex-fill">
-          <Routes>
-            <Route path="/" element={<Home />} />
-           
-           
-            <Route path="/categoria/:categoria" element={<Products />} />
-           
-           
-            <Route path="/product/:id" element={<ProductDetail />} />
-          </Routes>
-        </main>
+        {/* Rutas sin layout */}
+        <Route path="/login" element={<Login />} />
 
-       
-        <Footer />
-
-      </div>
+      </Routes>
     </Router>
   );
 };
