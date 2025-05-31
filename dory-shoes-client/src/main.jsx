25ae1814c905dcb140c4fdd3e5b3e6173ff63be8
Lauzari <mlauramoyano1985@gmail.com>
2025-05-31 05:14:19 -0300

    correction browser

--- dory-shoes-client/src/main.jsx
+++ dory-shoes-client/src/main.jsx
@@ -1,14 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css'
-import App from './App.jsx'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+import './index.css';
+import App from './App.jsx';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// Importar tus contextos
+import CartProvider from './components/Service/CartContext/CartContext';
+import { UserProvider } from './components/Service/usercontext/UserContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+      <UserProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </UserProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
