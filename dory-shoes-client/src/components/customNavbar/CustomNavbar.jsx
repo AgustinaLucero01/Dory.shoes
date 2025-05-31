dc0e6a79bcf187cdbde64ea761700c1d9a5dcd65
Lauzari <mlauramoyano1985@gmail.com>
2025-05-31 05:05:36 -0300

    adding navigation

--- dory-shoes-client/src/components/customNavbar/CustomNavbar.jsx
+++ dory-shoes-client/src/components/customNavbar/CustomNavbar.jsx
@@ -6,7 +6,7 @@ import ProductSearch from "../productSearch/ProductSearch";
 import { useNavigate } from "react-router-dom";
 import { CartContext } from "../Service/CartContext/CartContext";
 import { useAuth } from "./../Service/usercontext/UserContext.jsx";
-import Cart from "../Cart/Cart";
+import Cart from "../cart/cart.jsx";
 import { Link } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard.jsx";
 
@@ -58,34 +58,36 @@ const CustomNavbar = (carritoCantidad) => {
             &times;
           </div>
           <Nav className="flex-column">
-            <Nav.Link href="#inicio" onClick={handleClose}>
+            <Nav.Link as={Link} to="/" onClick={handleClose}>
               Inicio
             </Nav.Link>
+
             <NavDropdown title="Productos" id="productos-dropdown">
-              <NavDropdown.Item href="#todos" onClick={handleClose}>
+              <NavDropdown.Item as={Link} to="/products" onClick={handleClose}>
                 Todos los productos
               </NavDropdown.Item>
-              <NavDropdown.Item href="#botas" onClick={handleClose}>
+              <NavDropdown.Item as={Link} to="/categoria/botas" onClick={handleClose}>
                 Botas
               </NavDropdown.Item>
-              <NavDropdown.Item href="#zapatillas" onClick={handleClose}>
+              <NavDropdown.Item as={Link} to="/categoria/zapatillas" onClick={handleClose}>
                 Zapatillas
               </NavDropdown.Item>
-              <NavDropdown.Item href="#zapatos" onClick={handleClose}>
+              <NavDropdown.Item as={Link} to="/categoria/zapatos" onClick={handleClose}>
                 Zapatos
               </NavDropdown.Item>
-              <NavDropdown.Item href="#pantuflas" onClick={handleClose}>
-                Pantuflas
-              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/categoria/pantuflas" onClick={handleClose}>
+                Pantuflas </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#nosotros" onClick={handleClose}>
+            <Nav.Link as={Link} to="/about" onClick={handleClose}>
               Nosotros
             </Nav.Link>
-            <Nav.Link href="#faq" onClick={handleClose}>
+            <Nav.Link as={Link} to="/faq" onClick={handleClose}>
               Preguntas frecuentes
             </Nav.Link>
-            <Nav.Link href="#contacto" onClick={handleClose}>
+            <Nav.Link as={Link} to="/contact" onClick={handleClose}>
               Contacto
+
+
             </Nav.Link>
             {user && user.role === "admin" && (
               <Link to="/Dashboard" className="nav-link" onClick={handleClose} >
@@ -95,7 +97,8 @@ const CustomNavbar = (carritoCantidad) => {
           </Nav>
         </Navbar.Collapse>
 
-        <Navbar.Brand href="#home">
+
+        <Navbar.Brand as={Link} to="/" onClick={handleClose}>
           <img
             src="../../../images/DoryShoes-Logo.jpg"
             className="d-inline-block align-top"
@@ -111,16 +114,32 @@ const CustomNavbar = (carritoCantidad) => {
             />
           </div>
         )}
-        <div className="header-icons">
+       
+
+<div className="header-icons">
           <FaSearch className="icon" onClick={toggleSearch} />
-          <FaUser className="icon" />
-          <div className="count-product">
-            <span id="count-product" onClick={() => setActive(!active)}> {Number(countProduct) || 0}</span>
+          <FaUser    className="icon"
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              if (user) {
+                navigate(`/editProfile/${user.id}`);
+              } else {
+                navigate("/login");
+              }
+              navigate("/login");
+            }} />
+          <div className="count-product" onClick={() => setActive(!active)}>
+            <FaShoppingCart className="icon" />
+            <span id="count-product">
+              {Number(countProduct) || 0}
+            </span>
           </div>
           {active && <Cart />}
+
         </div>
-      </Container>
-    </Navbar>
+
+    </Container>
+    </Navbar >
   );
 };
 
